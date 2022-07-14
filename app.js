f0e98dad65581103915f0b7835e9cfa28b535aff
Flavio Lucas <flavio.silva@facens.br>
2022-07-14 18:32:18 -0300

    added camera toggle

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+
+const CameraEnum = {
+    USER: 'user',
+    ENVIRONMENT: 'environment',
+}
+
+const currentFacingMode = CameraEnum.USER;
+
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: { exact: currentFacingMode } }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
@@ -7,18 +15,28 @@ const cameraView = document.querySelector("#camera--view"),
     cameraTrigger = document.querySelector("#camera--trigger")
 // Access the device camera and stream to cameraView
 function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            stream = null
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error("Oops. Something is broken.", error);
+        });
 }
+
+async function toggleCamera() {
+    if (constraints.video.facingMode.exact === CameraEnum.USER) {
+        currentFacingMode = CameraEnum.ENVIRONMENT;
+    }
+    if (constraints.video.facingMode.exact === CameraEnum.ENVIRONMENT) {
+        currentFacingMode = CameraEnum.USER;
+    }
+}
+
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
+cameraTrigger.onclick = function () {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
